d8fd1c898ea3139627fdb0870a08c68882cc272a
anuxr4g <anun20003@gmail.com>
2024-09-19 11:08:11 +0530

    Initial Commit

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../Assets/logo.png'; // Adjust the path as necessary
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
+    <nav className="flex items-center justify-between p-4 bg-[#121212] text-white">
       <div className="flex items-center">
         <img src={logo} alt="Logo" className="h-12 mr-4" />
         <h1 className="text-xl md:text-2xl">My E-commerce Site</h1>
